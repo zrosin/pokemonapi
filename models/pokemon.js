adf9af77286664d6c2a1fdfa5409500e0faacb3f
Easton Pillay <easton@planeteaston.com>
2021-03-28 17:58:11 -0500

    Added schema and initial data.

--- models/pokemon.js
+++ models/pokemon.js
@@ -1,2 +1,14 @@
 const mongoose = require("../db");
 
+const PokemonSchema = new mongoose.Schema({
+    pokedexNumber: {type: Number, required: true, unique: true},
+    name: {type: String},
+    height: {type: Number},
+    weight: {type: Number},
+    types: {type: [String]},
+    abilities: {type: [String]}
+})
+
+const Pokemon = mongoose.model('Pokemon', PokemonSchema);
+
+module.exports = Pokemon
