c77a473b42990f4b02de8ea09425c222ec7fe58d
zrosin <77403498+zrosin@users.noreply.github.com>
2021-04-03 01:56:28 -0500

    Completed Tests, More robust routes.

--- api/pokemon.js
+++ api/pokemon.js
@@ -21,7 +21,7 @@ router.get('/id/:id', function(req, res) {
     //console.log('id = ' + req.params.id);
     Pokemon.findById(req.params.id, function(err, pokemon) {
         if (err) {
-            res.status(400).json({'message': `error: ${err}`});
+            res.status(404).json({'message': `error: ${err}`});
         }
         else if (pokemon) {
             res.json(pokemon);
@@ -32,12 +32,29 @@ router.get('/id/:id', function(req, res) {
     });
 });
 
-// Happy cases work. Unhappy cases fail with empty array. Probably pluralization.
+
+// Get pokemon by pokedex number
+// http://localhost:8000/api/pokemon/pokedex/2
+router.get('/pokedex/:pokedex', function(req, res) {
+    Pokemon.findOne({ pokedexNumber: { $eq:  req.params.pokedex} } , function(err, pokemon) {
+        if (err) {
+            console.log(err);
+            res.status(404).json({'message': `error: ${err}`});
+        }
+        else if (pokemon) {
+            res.json(pokemon);
+        }
+        else {
+            res.status(404).json({pokemon, 'message': 'Did not find Pokemon with Pokedex Number ' + req.params.pokedex}); 
+        }
+    });
+});
+
 // Get all pokemon with given type
 // http://localhost:8000/api/pokemon/type/grass
 router.get('/type/:types', function(req, res) {
-    console.log('types = ' + req.params.types);
-    console.log('{ types: {  $in: [' + req.params.types + '] } } }');
+    //console.log('types = ' + req.params.types);
+    //console.log('{ types: {  $in: [' + req.params.types + '] } } }');
     
     Pokemon.find({ types: { $in: [req.params.types] } } , function(err, pokemon) {
         if (err) {
@@ -53,64 +70,70 @@ router.get('/type/:types', function(req, res) {
     });
 });
 
-// Tested happy cases
 // Add new pokemon
 // http://localhost:8000/api/pokemon/   
 // POST a JSON with Content-Type: application/json
 router.post('/', function(req, res) {
- 
-    // Create a new pokemon from JSON request body
-    const pokemon = new Pokemon({
-        pokedexNumber: req.body.pokedexNumber,
-        name: req.body.name,
-        height: req.body.height,
-        weight: req.body.weight,
-        types: req.body.types,
-        abilities: req.body.abilities
-    });
- 
-    // Save pokemon and return it as JSON
-    pokemon.save(function(err, poke) {
-        if (err) {
-            res.status(400).send(err);
-        }
-        else {
-            res.status(201).json(poke);
-        }
-    });
+    if(req.body.pokedexNumber !== null && req.body.pokedexNumber > 0 && !isNaN(req.body.pokedexNumber)){
+        // Create a new pokemon from JSON request body
+        const pokemon = new Pokemon({
+            pokedexNumber: req.body.pokedexNumber,
+            name: req.body.name,
+            height: req.body.height,
+            weight: req.body.weight,
+            types: req.body.types,
+            abilities: req.body.abilities
+        });
+    
+        // Save pokemon and return it as JSON
+        pokemon.save(function(err, pokemon) {
+            if (err) {
+                res.status(400).send(err);
+            }
+            else {
+                res.status(201).json(pokemon);
+            }
+        });
+    }
+    else {
+        res.status(404).json({'message': 'Incorrectly formatted pokemon'});
+    }
 });
 
-// Untested
-// Update existing student
+// Update existing pokemon
 // http://localhost:8000/api/pokemon/id/6063cdb5f0af1b48c8a5218d
 router.put('/id/:id', function(req, res) {
- 
-    // Update the student with values from the request
-    Pokemon.updateOne({ _id: req.params.id }, req.body, function(err, result) {
-        if (err) {
-            res.status(400).send(err);
-        }
-        else if (result.n === 0) {
-            res.status(404).json({ message: 'Pokemon not found' });
-        }
-        else {
-            res.sendStatus(204);
-        }
-    });
+    if(req.body.pokedexNumber !== null){
+        // Update the student with values from the request
+        Pokemon.updateOne({ _id: req.params.id }, req.body, function(err, result) {
+            if (err) {
+                res.status(400).send(err);
+            }
+            else if (result.n === 0) {
+                res.status(404).json({ message: 'Pokemon not found' });
+            }
+            else {
+                res.sendStatus(204);
+            }
+        });
+    }
+    else {
+        res.status(404).json({ message: 'Pokedex Number is a required field' });
+    }
 });
 
-// Tested happy cases.
-// Delete existing student with the given ID in the URL
+
+// Delete existing pokemon with the given ID in the URL
 // http://localhost:8000/api/pokemon/id/6063cdb5f0af1b48c8a5218d
 router.delete('/id/:id', function(req, res) {
  
-    // Make sure the student ID was sent
+    // Make sure the pokemon ID was sent
     if (req.params.id === undefined) {
         res.status(400).json({ message: 'Pokemon ID is missing' });
         return next();
     }
  
-    // Delete this student
+    // Delete this pokemon
     Pokemon.deleteOne({ _id: req.params.id }, function(err, result) {
         if (err) {
             res.status(400).send(err);
@@ -122,14 +145,6 @@ router.delete('/id/:id', function(req, res) {
             res.sendStatus(204);
         }
     });
-});
-
-// Return 404 if given no ID.
-router.delete('/', function(req, res) {
-    res.status(404).json({ message: 'Pokemon not found' });
-});
-router.delete('/id/', function(req, res) {
-    res.status(404).json({ message: 'Pokemon not found' });
 });
 
 
--- test/pokemon.js
+++ test/pokemon.js
@@ -20,7 +20,7 @@ describe('Pokemon API', () => {
         // Clear array
         testPokemonDocs.length = 0;       
 
-        // Add test students
+        // Add test pokemon
         for (const p of testPokemon) {
             const pokemon = new Pokemon(p);
             const pokemonDoc = await pokemon.save();
@@ -146,7 +146,7 @@ describe('Pokemon API', () => {
     describe('GET /pokemon', () => {
         it('should get all the pokemon', (done) => {
             chai.request(server)
-                .get('/api/pokemon')
+                .get('/api/pokemon/')
                 .end((err, res) => {
                     res.should.have.status(200);
                     res.body.should.be.a('array');
@@ -156,9 +156,10 @@ describe('Pokemon API', () => {
         });
     });
 
-    describe('GET /:id pokemon', () => {
+    describe('GET /pokemon/id/:id', () => {
         it('should get a pokemon by id', (done) => {
             const pokemon = testPokemonDocs[0];
+
             chai.request(server)
                 .get('/api/pokemon/id/' + pokemon._id)
                 .end((err, res) => {
@@ -169,10 +170,245 @@ describe('Pokemon API', () => {
                     res.body.should.have.property('height').eql(pokemon.height);
                     res.body.should.have.property('weight').eql(pokemon.weight);
                     res.body.should.have.property('types').eql(pokemon.types);
-                    res.body.should.have.property('_id').eql(pokemon.id.toString());
+                    res.body.should.have.property('abilities').eql(pokemon.abilities);
+                    res.body.should.have.property('_id').eql(pokemon._id.toString());
+                    done();
+                });
+        });
+        it('should return 404 with a bad id', (done) => {
+            chai.request(server)
+                .get('/api/pokemon/id/badid/')
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
+        
+    });
+
+    describe('GET /pokemon/pokedex/:pokedex', () => {
+        it('should get a pokemon by its pokedex number', (done) => {
+            const pokemon = testPokemonDocs[5];
+
+            chai.request(server)
+                .get('/api/pokemon/pokedex/' + pokemon.pokedexNumber)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('pokedexNumber').eql(pokemon.pokedexNumber);
+                    res.body.should.have.property('name').eql(pokemon.name);
+                    res.body.should.have.property('height').eql(pokemon.height);
+                    res.body.should.have.property('weight').eql(pokemon.weight);
+                    res.body.should.have.property('types').eql(pokemon.types);
+                    res.body.should.have.property('abilities').eql(pokemon.abilities);
+                    res.body.should.have.property('_id').eql(pokemon._id.toString());
+                    done();
+                });
+        });
+        it('should return 404 with a bad pokedex number', (done) => {
+            chai.request(server)
+                .get('/api/pokemon/pokedex/11/')
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
+    });
+
+    describe('GET /pokemon/type/:type', () => {
+        it('should get pokemon based on type', (done) => {
+            const pokemon = testPokemonDocs[1];
+
+            chai.request(server)
+                .get('/api/pokemon/type/' + pokemon.types[0])
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+                    res.body.length.should.be.eql(3);
+                    res.body[0].types[0].should.be.eql("grass");
+                    done();
+                });
+        });
+        it('should return 404 on bad type', (done) => {
+            chai.request(server)
+                .get('/api/pokemon/type/notatype/')
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    res.body.should.be.a('object');
+                    res.body.pokemon.should.be.eql([]);
+                    done();
+                });
+        });
+    });
+
+    describe('POST /pokemon/', () => {
+        it('should create a new pokemon', (done) => {
+            const pokemon = testPokemonDocs[9];
+            //pokedex numbers are unique
+            pokemon.pokedexNumber = 11;
+
+            chai.request(server)
+                .post('/api/pokemon/')
+                .send(pokemon)
+                .end((err, res) => {
+                    res.should.have.status(201);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('pokedexNumber').eql(pokemon.pokedexNumber);
+                    res.body.should.have.property('name').eql(pokemon.name);
+                    res.body.should.have.property('height').eql(pokemon.height);
+                    res.body.should.have.property('weight').eql(pokemon.weight);
+                    res.body.should.have.property('types').eql(pokemon.types);
+                    res.body.should.have.property('abilities').eql(pokemon.abilities);
+                    done();
+                });
+        });
+        it('should not overwrite a pokemon', (done) => {
+            const pokemon = testPokemonDocs[9];
+
+            chai.request(server)
+                .post('/api/pokemon/')
+                .send(pokemon)
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    done();
+                });
+        });
+        it('should not accept an incorrectly formatted pokemon', (done) => {
+            pokemon = testPokemonDocs[6];
+            //pokedex numbers are required
+            pokemon.pokedexNumber = null;
+
+            chai.request(server)
+                .post('/api/pokemon/')
+                .send(pokemon)
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    done();
+                });
+        });
+        it('should not accept an incorrectly formatted pokemon', (done) => {
+            pokemon = testPokemonDocs[6];
+            //pokedex numbers are required
+            pokemon.pokedexNumber = -12;
+
+            chai.request(server)
+                .post('/api/pokemon/')
+                .send(pokemon)
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    done();
+                });
+        });
+        it('should not accept an incorrectly formatted pokemon', (done) => {
+            pokemon = testPokemonDocs[6];
+            //pokedex numbers are required
+            pokemon.pokedexNumber = "asdf";
+
+            chai.request(server)
+                .post('/api/pokemon/')
+                .send(pokemon)
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    done();
+                });
+        });
+    });
+
+    describe('PUT /pokemon/id/:id', () => {
+        it('should update a pokemon', (done) => {
+            const pokemon = testPokemonDocs[4];
+            pokemon.name = "Chad";
+            pokemon.weight = 49.1;
+            pokemon.height = 123.456;
+            pokemon.types = ["water", "anger"];
+
+            chai.request(server)
+                .put('/api/pokemon/id/' + testPokemonDocs[4]._id)
+                .send(pokemon)
+                .end((err, res) => {
+                    res.should.have.status(204);
+
+                    chai.request(server)
+                        .get('/api/pokemon/id/' + testPokemonDocs[4]._id)
+                        .end((err, res) => {
+                            res.body.should.have.property('pokedexNumber').eql(pokemon.pokedexNumber);
+                            res.body.should.have.property('name').eql(pokemon.name);
+                            res.body.should.have.property('height').eql(pokemon.height);
+                            res.body.should.have.property('weight').eql(pokemon.weight);
+                            res.body.should.have.property('types').eql(pokemon.types);
+                            res.body.should.have.property('abilities').eql(pokemon.abilities);
+                            done();
+                        });
+                });
+        });
+        it('should update a pokemon with no changes', (done) => {
+            const pokemon = testPokemonDocs[4];
+
+            chai.request(server)
+                .put('/api/pokemon/id/' + testPokemonDocs[4]._id)
+                .send(pokemon)
+                .end((err, res) => {
+                    res.should.have.status(204);
+
+                    chai.request(server)
+                        .get('/api/pokemon/id/' + testPokemonDocs[4]._id)
+                        .end((err, res) => {
+                            res.body.should.have.property('pokedexNumber').eql(pokemon.pokedexNumber);
+                            res.body.should.have.property('name').eql(pokemon.name);
+                            res.body.should.have.property('height').eql(pokemon.height);
+                            res.body.should.have.property('weight').eql(pokemon.weight);
+                            res.body.should.have.property('types').eql(pokemon.types);
+                            res.body.should.have.property('abilities').eql(pokemon.abilities);
+                            done();
+                        });
+                });
+        });
+        it('should not update a pokemon with invalid parameters', (done) => {
+            const pokemon = testPokemonDocs[6];
+            pokemon.pokedexNumber = null;
+
+            chai.request(server)
+                .put('/api/pokemon/id/' + testPokemonDocs[6]._id)
+                .send(pokemon)
+                .end((err, res) => {
+                    res.should.have.status(404);
+
+                    chai.request(server)
+                        .get('/api/pokemon/id/' + testPokemonDocs[6]._id)
+                        .end((err, res) => {
+                            res.body.should.have.property('pokedexNumber').not.eql(pokemon.pokedexNumber);
+                            done();
+                        });
+                });
+        });
+    });
+
+    describe('DELETE /pokemon/id/:id', () => {
+        it('should delete a pokemon', (done) => {
+            const pokemon = testPokemonDocs[8];
+
+            chai.request(server)
+                .delete('/api/pokemon/id/' + pokemon._id)
+                .end((err, res) => {
+                    res.should.have.status(204);
+
+                    chai.request(server)
+                        .get('/api/pokemon/id/' + pokemon._id)
+                        .end((err, res) => {
+                            res.should.have.status(404);
+                            done();
+                        });
+                });
+        });
+        it('should not delete a pokemon with an invalid id', (done) => {
+            chai.request(server)
+                .delete('/api/pokemon/id/badid')
+                .end((err, res) => {
+                    res.should.have.status(400);
                     done();
                 });
         });
     });
 });
-
