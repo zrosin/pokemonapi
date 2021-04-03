23c2e31d56cca99c6d757afdb4ea28badf7c53c9
Easton Pillay <easton@planeteaston.com>
2021-04-03 10:50:06 -0500

    Resolved depreciation warning.

--- db.js
+++ db.js
@@ -2,7 +2,11 @@
 
 const mongoose = require("mongoose");
 
+// .sets are for resolving depreciation warnings. We aren't doing anything that complex anyway...
+// https://mongoosejs.com/docs/deprecations.html
 mongoose.set("useUnifiedTopology", true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
 mongoose.connect("mongodb://localhost/pokemondb", {useNewUrlParser: true});
 
 module.exports = mongoose
