d3262ee02e3fcdfa1949459a88aa92cb33951294
jtucker9harding <jtucker9@harding.edu>
2021-04-20 23:52:58 -0500

    added initial implementation of React-Routing

--- react/src/App.js
+++ react/src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import { BrowserRouter as Router, Switch, Route, Link, useParams } from 'react-router-dom';
+import React, { useState } from 'react';
+/* function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +20,72 @@ function App() {
         </a>
       </header>
     </div>
+  );
+}
+
+export default App; */
+
+
+function Navbar() {
+  return (
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/widgets">Widgets</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+    </ul>
+  );
+}
+
+function App() {
+
+  function About() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>This is a test.</p>
+      </div>
+    );
+  }
+
+  function Homepage() {
+    return (
+      <div>
+        <h1>Homepage</h1>
+        <p>This is another test</p>
+      </div>
+    );
+  }
+
+  function NotFound() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>This is also test</p>
+      </div>
+    );
+  }
+
+  return (
+    <Router>
+      <Navbar></Navbar>
+      <Switch>     
+        <Route path="/about/:option" children={<About />} />
+          <About />
+        <Route exact path="/">
+          <Homepage />
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
+	  </Switch>   
+	</Router>
+
   );
 }
 
