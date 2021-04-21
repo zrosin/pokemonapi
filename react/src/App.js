3dfa422b98270b652fb2c1baec2b29c0c0a51e8e
jtucker9harding <jtucker9@harding.edu>
2021-04-21 11:35:29 -0500

    Routing Initial Implementation (with example from class)
    The class example was modified to deal with class components, rather than function components.

--- react/src/App.js
+++ react/src/App.js
@@ -30,7 +30,7 @@ function Navbar() {
   return (
     <ul>
       <li>
-        <Link to="/">Home</Link>
+        <Link to="/home">Home</Link>
       </li>
       <li>
         <Link to="/widgets">Widgets</Link>
@@ -42,51 +42,65 @@ function Navbar() {
   );
 }
 
-function App() {
-
-  function About() {
-    return (
-      <div>
-        <h1>About</h1>
-        <p>This is a test.</p>
-      </div>
-    );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
   }
 
-  function Homepage() {
+  render() {
     return (
+      <Router>
+        <Navbar></Navbar>
+        <Switch>     
+          <Route path="/about">
+            <About />
+          </Route>
+	      </Switch>
+        <Switch>
+          <Route path="/home">
+            <Home />
+          </Route>
+        </Switch>
+        <Switch>
+          <Route path="/widgets">
+            <Widgets />
+          </Route>
+        </Switch>      
+	    </Router>);
+    }
+}
+
+class About extends React.Component {
+
+  render() {
+    return(
       <div>
-        <h1>Homepage</h1>
         <p>This is another test</p>
       </div>
     );
   }
+}
 
-  function NotFound() {
-    return (
+class Widgets extends React.Component {
+
+  render() {
+    return(
       <div>
-        <h1>About</h1>
-        <p>This is also test</p>
+        <p>This is an extra test</p>
       </div>
     );
   }
+}
 
-  return (
-    <Router>
-      <Navbar></Navbar>
-      <Switch>     
-        <Route path="/about/:option" children={<About />} />
-          <About />
-        <Route exact path="/">
-          <Homepage />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-	  </Switch>   
-	</Router>
+class Home extends React.Component {
 
-  );
+  render() {
+    return(
+      <div>
+        <p>This is a test</p>
+      </div>
+    );
+  }
 }
 
 export default App;
