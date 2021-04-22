722b183597623df164b2942015cbc39157db2538
Easton Pillay <easton@planeteaston.com>
2021-04-22 10:50:57 -0500

    Enabled CORS (not that we __really__ need it).

--- webapi/server.js
+++ webapi/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
-const app = express()
+const app = express();
+
 
 // setup middleware.
 app.use(morgan('dev', { skip: (req, res) => process.env.NODE_ENV && process.env.NODE_ENV.trim() === 'test' }));
@@ -9,7 +11,9 @@ app.use(express.json());
 
 // API routes
 app.use("/", express.static("static"));
-app.use("/api/pokemon", require("./api/pokemon"));
+
+// CORS is only enabled for API routes. There isn't a good reason why one would need to grab our static assets from another origin.
+app.use("/api/pokemon", cors(), require("./api/pokemon"));
 
 app.listen(8000, () => console.log("Pokémon API is listening on port 8000!"));
 
