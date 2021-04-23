12f2f9f48bbfb34ad600b39723519b5088e39bd7
Easton Pillay <easton@planeteaston.com>
2021-04-23 08:34:14 -0500

    Now we can pass a database connection string as an env var.

--- webapi/db.js
+++ webapi/db.js
@@ -4,9 +4,10 @@ const mongoose = require("mongoose");
 
 // .sets are for resolving depreciation warnings. We aren't doing anything that complex anyway...
 // https://mongoosejs.com/docs/deprecations.html
+const mongoConnectionString = process.env.MONGO_CONNECTION_STRING ? process.env.MONGO_CONNECTION_STRING : "mongodb://localhost/pokemondb";
 mongoose.set("useUnifiedTopology", true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
-mongoose.connect("mongodb://localhost/pokemondb", {useNewUrlParser: true});
+mongoose.connect(mongoConnectionString, {useNewUrlParser: true});
 
 module.exports = mongoose
