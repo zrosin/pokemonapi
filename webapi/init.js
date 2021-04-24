7a9a0b85e18131c3973188abb01060baa83c0d14
Easton Pillay <easton@planeteaston.com>
2021-04-24 18:27:05 -0500

    The great schema change is upon us.

--- webapi/init.js
+++ webapi/init.js
@@ -5,24 +5,33 @@
 const {Pokemon, MoveSet, Move, Ability} = require("./models/pokemon");
 // const  = require("./models/moves");
 const fs = require('fs');
+const fetch = require('node-fetch');
 
+async function fetchResults(path) {
+    return fetch(path)
+      .then(res=>{return res.buffer();})
+      .then(blob=>{;
+        // console.log(blob);
+        return blob;
+      })
+  }
 
 async function initPkmn(dex) {
     await Pokemon.deleteMany({});
     let url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"
     //path = url + "1" + ".png";
-
+    console.log("Downloading images and importing Pokemon, this may take a moment.");
     await Promise.all(dex.map(async (i) => {
         const pkmn = new Pokemon(i);
         path = url + pkmn.pokedexNumber + ".png";
 
-        pkmn.img = await ((path) => fetch(path).then(res => res.buffer()))
-        pkmn.img.contentType = "image/png"
+        pkmn.image = await fetchResults(path);
+        pkmn.image.contentType = "image/png"
 
         await pkmn.save();
     })
     );
-    console.log("Database initialized with the National Pokedex!")
+    console.log("National Pokedex import complete!")
 }
 
 async function initAbilities(abilities) {
@@ -39,7 +48,6 @@ async function initMoves(moveSet, moves) {
     await MoveSet.deleteMany({});
     await Move.deleteMany({});
 
-    console.log("Importing moveSets, this may take a moment...")
     await Promise.all(moveSet.map(async (i) => {
         // console.log(i.pokedexNumber);
         const newMoveSet = new MoveSet(i);
@@ -58,20 +66,26 @@ async function initMoves(moveSet, moves) {
 // (The data was in 3NF, and I didn't completely denormalize, so Mongo isn't running as fast as it would like. Oh well.)
 // That project is licensed under the MIT License, but of course the contents (but not the formats) of the files are (c) Nintendo/Creatures/Game Freak.
 
+let promises = [];
 const rawPokemon = fs.readFileSync("test/data/basic.json");
 const natDex = JSON.parse(rawPokemon);
-initPkmn(natDex);
+promises.push(initPkmn(natDex));
 
 const rawAbility = fs.readFileSync("test/data/abilities.json");
 const abilities = JSON.parse(rawAbility);
-initAbilities(abilities);
+promises.push(initAbilities(abilities));
 
 const rawMoveSets = fs.readFileSync("test/data/moveset.json");
 const moveSets = JSON.parse(rawMoveSets);
 const rawMoves = fs.readFileSync("test/data/moves.json");
 const moves = JSON.parse(rawMoves);
-initMoves(moveSets, moves).then(() => {
-    console.log("Database initialized! Have fun! 🎉");
-    process.exit(); 
-});
+promises.push(initMoves(moveSets, moves));
 
+async function finish(arr) {
+    await Promise.all(arr).then(() => {
+        console.log("Database initialized! Have fun! 🎉");
+        process.exit(); 
+    });
+}
+finish(promises)
+// process.exit();
--- webapi/models/pokemon.js
+++ webapi/models/pokemon.js
@@ -1,8 +1,9 @@
-// Schema and functions for dealing with a Pokemon.
+// Schemas for dealing with a Pokemon and related data about a Pokemon.
 
+const { Mongoose } = require("mongoose");
 const mongoose = require("../db");
 
-const PokemonSchema = new mongoose.Schema({
+const pokemonSchema = new mongoose.Schema({
     pokedexNumber: {type: Number, required: true, unique: true},
     name: {type: String},
     height: {type: Number},
@@ -17,9 +18,58 @@ const PokemonSchema = new mongoose.Schema({
     specialAttack: {type: Number},
     specialDefense: {type: Number},
     speed: {type: Number},
-    hiddenAbility: {type: String}
+    hiddenAbility: {type: String},
+    image: {type: Buffer}
+});
+
+// imgurl shouldn't be persisted to the database (what if the route ever changed? That'd be a pain!) so a virtual property is used.
+// https://mongoosejs.com/docs/guide.html#virtuals
+pokemonSchema.virtual('imgurl').get(function () {return `/api/pokemon/img/${this.pokedexNumber}`});
+
+const Pokemon = mongoose.model('Pokemon', pokemonSchema);
+
+// the collection that stores what moves each pokemon can do. 
+// each record has a pokedex number (which equals what pokemon can use this move),
+// a move id (corresponding to the id attribute in the Move schema), and 
+// learnedVia (how they can get the move)
+// and the minimum level (which we don't use for anything, but would be helpful to a end user maybe).
+const moveSetSchema = new mongoose.Schema({
+    pokedexNumber: {type: Number},
+    moveId: {type: Number},
+    learnedVia: {type: String},
+    level: {type: Number}
 })
 
-const Pokemon = mongoose.model('Pokemon', PokemonSchema);
+const MoveSet = mongoose.model('MoveSet', moveSetSchema);
 
-module.exports = Pokemon
+// the collection that stores a move. of interest are:
+// id == arbitrary integer for tracking unique moves. we don't use a objectID since that is random.
+// damageClass: one of "physical", "status", "special".
+// flavorText: a description of a move.
+
+const moveSchema = new mongoose.Schema({
+    id: {type: Number, unique: true},
+    name: {type: String},
+    type: {type: String}, 
+    power: {type: Number, max: 250},
+    pp: {type: Number},
+    accuracy: {type: Number},
+    damageClass: {type: String},
+    flavorText: {type: String},
+})
+
+const Move = mongoose.model('Move', moveSchema);
+
+// Collection for containing ability metadata.
+// generation == the game generation it was first introduced.
+
+const abilitySchema = new mongoose.Schema({
+    name: {type: String},
+    generation: {type: Number, max: 8, min: 1},
+    flavorText: {type: String}
+});
+
+
+const Ability = new mongoose.model('Ability', abilitySchema);
+
+module.exports = {Pokemon, Move, MoveSet, Ability}
