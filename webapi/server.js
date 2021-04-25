04bc57845801be7d08fcf6a62890bc6604b0399c
Easton Pillay <easton@planeteaston.com>
2021-04-25 12:27:52 -0500

    Server fixes.

--- webapi/server.js
+++ webapi/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const morgan = require('morgan');
 const cors = require('cors');
 
@@ -9,11 +10,15 @@ const app = express();
 app.use(morgan('dev', { skip: (req, res) => process.env.NODE_ENV && process.env.NODE_ENV.trim() === 'test' }));
 app.use(express.json());
 
-// API routes
-app.use("/", express.static("static"));
-
 // CORS is only enabled for API routes. There isn't a good reason why one would need to grab our static assets from another origin.
 app.use("/api/pokemon", cors(), require("./api/pokemon"));
+
+// static file handling, and ensures React Router is not interfered with.
+
+app.use(express.static("static"));
+app.get('*', (req, res) => {                       
+    res.sendFile(path.resolve(__dirname, 'static', 'index.html'));                               
+});
 
 app.listen(8000, () => console.log("Pokémon API is listening on port 8000!"));
 
