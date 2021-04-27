3be88e0035798a660a7bc5fa55ababd389167951
jtucker9harding <jtucker9@harding.edu>
2021-04-27 18:14:01 -0500

    Added initial implementation for user authentication on frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,30 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link, } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link, Redirect} from 'react-router-dom';
 import React, { useState, useEffect} from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
 import Nav from 'react-bootstrap/Nav';
 import { DetailedPokemon } from './DetailedPokemon';
 
 
 
 function Navigation() {
+  if(sessionStorage.getItem("jwt")) {
+    return(
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand as={Link} to="/">Pokédex!</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link as={Link} to="/">Main Page</Nav.Link>
+        </Nav>
+      </Navbar>
+    );
+  }
   return (
   <Navbar bg="dark" variant="dark">
-    <Navbar.Brand as={Link} to="/">Pokédex!</Navbar.Brand>
     <Nav className="mr-auto">
-      <Nav.Link as={Link} to="/">Main Page</Nav.Link>
+      <Nav.Link as={Link} to="/login">Log in</Nav.Link>
       {/* links for disabled routes, uncomment routes to make sure these do anything if we want them back. */}
       {/* <Nav.Link as={Link} to="/updatepokemon">Update Pokemon</Nav.Link>
       <Nav.Link as={Link} to="/getpokemon">Get Pokemon</Nav.Link>
@@ -23,16 +35,98 @@ function Navigation() {
   );
 }
 
-function App() {
-    return (
-      <Router>
-        <Navigation></Navigation>
+function Login() {
+  const [UserName, setUserName] = useState("");
+  const [PassWord, setPassWord] = useState("");
+  const [HasAttempted, setHasAttempted] = useState(false);
+  const [alert, setAlert] = useState("");
+
+  useEffect(() => {
+    if(HasAttempted) {
+      async function authenticate() {
+        const response = await fetch("/api/auth", {
+          method: "POST",
+          body: JSON.stringify({ 
+             username: UserName,
+             password: PassWord })
+        });
+        console.log(response);
+        if (!('message' in response)) {
+          alert("Login successful!");
+          sessionStorage.set("jwt", response);
+        }
+        else {
+          setAlert(response.message);
+        }
+      }
+      authenticate();
+      setHasAttempted(false);
+    }
+  }, [HasAttempted, alert, UserName, PassWord]);
+
+  function AlertBox() {
+    if(alert !== "") {
+      return(
+        <Alert variant={'danger'}>
+          {alert}
+        </Alert>
+      );
+    }
+    else {
+      return (
+        ""
+      );
+    }
+  }
+
+  return (
+    <div className = "login">
+      <AlertBox />
+      <Form onSubmit={() => setHasAttempted(true)}>
+        <Form.Row>
+          <Form.Group>
+            <Form.Label>Enter Username:</Form.Label>
+            <Form.Control as="input" onChange={e => setUserName(e.target.value)} type="text" value={UserName}></Form.Control>
+          </Form.Group>
+        </Form.Row>
+        <Form.Row>
+          <Form.Group>
+            <Form.Label>Enter Password:</Form.Label>
+            <Form.Control as="input" onChange={e => setPassWord(e.target.value)} type="text" value={PassWord}></Form.Control>
+          </Form.Group>
+        </Form.Row>
+        <Button variant="primary" type="submit">Login</Button>
+      </Form>
+    </div>
+  );
+}
+
+function AuthenticatedRoutes() {
+  if(sessionStorage.getItem("jwt")) {
+    return(
         <Switch>
           <Route path="/pokemon/:dexNum">
             <DetailedPokemon />
           </Route>
           <Route exact path={["/", "/pokemon"]}>
-            <GetPokemonForMainPage/>
+            <GetPokemonForMainPage />
+          </Route>
+        </Switch>
+    );
+  }
+  return (
+    <Redirect to='/login' />
+  );
+}
+
+function App() {
+    return (
+      <Router>
+        <Navigation></Navigation>
+        <AuthenticatedRoutes />
+        <Switch>
+          <Route path="/login">
+            <Login />
           </Route>
         </Switch>
         {/* disabled routes. uncomment and fix imports to reenable. */}
