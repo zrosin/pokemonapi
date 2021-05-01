605a34a6c282140821292fdc386e0a6db9d9d1ce
zrosin <77403498+zrosin@users.noreply.github.com>
2021-05-01 04:32:34 -0500

    Almost done team builder

--- frontend/src/TeamBuilder.js
+++ frontend/src/TeamBuilder.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { checkNull } from './App';
+import Table from 'react-bootstrap/Table';
+import Form from 'react-bootstrap/Form';
 
 //This function sends a proper request for an image with the authentication header.
 function Image(props) {
@@ -28,10 +32,23 @@ function Image(props) {
     <img className="team_image" src={imageUrl} alt={props.alt} />
   );
 }
+
+
+
 export function TeamBuilder() {
     const [pokemonSelected, setPokemon] = useState([]);
     const [analysis, setAnalysis] = useState("");
     const [runAnalysis, addRunAnalysis] = useState(0);
+    const [selectedPoke, setSelectedPokemon] = useState(null);
+    const [loadedPoke, setLoadedPoke] = useState(null);
+    const [DetailedInfo, setDetailedInfo] = useState("");
+    const [moves, changeMove] = useState([]);
+    const [newPokemon, changeNewPokemon] = useState(null);
+    const [pokeList, changePokeList] = useState(null);
+    const [moveList, changeMoveList] = useState([]);
+
+    
+
     
     
     //pokemonSelected needs to be a default mon that just has no info or something.
@@ -44,7 +61,8 @@ export function TeamBuilder() {
           let result = response;
           setPokemon(result.comp);
         }
-        getTeamPokemon()
+        
+        getTeamPokemon();
     }, []);
 
     function TeamDisplay() {
@@ -52,26 +70,174 @@ export function TeamBuilder() {
 
         if (pokemonSelected) {
             
+            //Rerenders pokemon on button press
             Team = pokemonSelected.map((poke) => (
-                <div id={poke.pokedexNum} className="team_builder_pokemon">
-                    <div>
-                        <Image  url={poke.pokemon.imgurl} alt={poke.pokemon.name + "image"} />
+                <Button id={poke.pokedexNum} variant="outline-primary" className="team_builder_pokemon_button" onClick={(e) => {setSelectedPokemon(poke); e.preventDefault();}}>
+                    <div id={poke.pokedexNum} className="team_builder_pokemon">
+                        <div>
+                            <Image  url={poke.pokemon.imgurl} alt={poke.pokemon.name + "image"} />
+                        </div>
+                        
+                        <div className="team_builder_pokemon_info">
+                            <h6>{poke.pokemon.name}</h6>
+                            <p>HP: {poke.pokemon.hp}</p>
+                            <p>Attack: {poke.pokemon.attack}</p>
+                            <p>Defense: {poke.pokemon.defense}</p>
+                        </div>
                     </div>
-                    
-                    <div className="team_builder_pokemon_info">
-                        <h6>{poke.pokemon.name}</h6>
-                        <p>HP: {poke.pokemon.hp}</p>
-                        <p>Attack: {poke.pokemon.attack}</p>
-                        <p>Defense: {poke.pokemon.defense}</p>
-                    </div>
-                    
-                </div>
+                </Button>
             ));
         }
 
         return (
             Team
         );
+    }
+
+    useEffect(() => {
+        async function getPokeList() {
+            let token = sessionStorage.getItem("jwt");
+              let response = await fetch("/api/pokemon/names/", {
+                headers: { "x-auth": token }}).then(r => r.json());
+              let result = response;
+              changePokeList(result);
+            }
+        getPokeList();
+    }, []);
+
+    
+
+    function PokeCard() {
+
+        useEffect(() => {
+            
+            async function getMoveList() {
+                let token = sessionStorage.getItem("jwt");
+                let response = await fetch("/api/pokemon/moveset/" + selectedPoke.pokemon.pokedexNumber, {
+                    headers: { "x-auth": token }}).then(r => r.json());
+                let result = response;
+                changeMoveList(result);
+                return;
+            }
+
+            async function getDetails() {
+              if (selectedPoke != null && selectedPoke != loadedPoke) {
+                let typeList = selectedPoke.pokemon.types[1] == null ? selectedPoke.pokemon.types[0] : selectedPoke.pokemon.types[0] + ", " + selectedPoke.pokemon.types[1];
+
+                let initialAbilityList = selectedPoke.pokemon.abilities.filter(checkNull);
+                let abilityList = "";
+
+                for (let ability of initialAbilityList) {
+                  if (initialAbilityList.indexOf(ability) === (initialAbilityList.length - 1)) {
+                    abilityList += ability;
+                  }
+                  else {
+                    abilityList += (ability + ", ");
+                  }
+                }
+
+                await getMoveList();
+
+                setDetailedInfo(
+                  <Card className="team_card">
+                    <h1 className="detailed_name">{selectedPoke.pokemon.name}</h1>
+                    <Image url={selectedPoke.pokemon.imgurl}/>
+                    <ul>
+                      <li><h4>Pokedex Number: {selectedPoke.pokemon.pokedexNumber}</h4></li><br />
+                      <li><h4>Height (m): {selectedPoke.pokemon.height}</h4></li><br />
+                      <li><h4>Weight (kg): {selectedPoke.pokemon.weight}</h4></li><br />
+                      <li><h4>Types: {typeList}</h4></li><br />
+                      <li><h4>Abilities: {abilityList}</h4></li><br />
+                      <p>
+                        <b>{selectedPoke.pokemon.flavorText}</b>
+                      </p>
+                      <Table>
+                        <thead>
+                          <tr>
+                            <th>HP</th>
+                            <th>Attack</th>
+                            <th>Defense</th>
+                            <th>Special Attack</th>
+                            <th>Special Defense</th>
+                            <th>Speed</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td>{selectedPoke.pokemon.hp}</td>
+                            <td>{selectedPoke.pokemon.attack}</td>
+                            <td>{selectedPoke.pokemon.defense}</td>
+                            <td>{selectedPoke.pokemon.specialAttack}</td>
+                            <td>{selectedPoke.pokemon.specialDefense}</td>
+                            <td>{selectedPoke.pokemon.speed}</td>
+                          </tr>
+                        </tbody>
+                      </Table>
+                    </ul>
+                    {/*form to edit moves*/}
+                    <Form>
+                        <label>
+                            Do not duplicate moves 
+                            <label>
+                                {selectMove(1)}
+                                {selectMove(2)}
+                                {selectMove(3)}
+                                {selectMove(4)}
+                            </label>
+                        </label>
+                        <Button onClick={updatePokemon}>Save</Button>
+                    </Form>
+                    {/*form to change pokemon*/}
+                    <Form>
+                        <label>
+                            Or pick a new pokemon:   
+                            {/* Not sure if I should sort by pokedex or alphabetical. 
+                            With a list so long I'm inclined to choose alphabet, 
+                            but you can change the API call I made if you desire. */}
+                            <select value={newPokemon} onChange={(e) => changeNewPokemon(e.target.value)}>
+                                {pokeList.pokemon.map((poke) => (
+                                    <option value={poke.id}>{poke.name}</option>
+                                ))}
+                            </select>
+                        </label>
+                    <Button onClick={changePokemon}>Save</Button>
+                    </Form>
+                  </Card>);
+                  setLoadedPoke(selectedPoke);
+              }
+          }
+          getDetails();
+
+          function selectMove(num) {
+              // I'm not sure if this is actually how you are supposed to access a prop array
+              // React and select dont get along well.
+            return (
+                <select value={moves[num]} onchange={(e) => changeMove(e.target.value)}>
+                    <option>None</option>
+                    {moveList.map((move) => (
+                    <option value={move.id}>{move.name}</option>
+                    ))}
+                </select>
+            )
+          }
+      });
+
+        return (
+            DetailedInfo
+        );
+    }
+
+    function updatePokemon() {
+        //See note in selectMove. You can also change value to be whatever is needed.
+        //New requested moves in moves which should be an array of 4 moves, though i might not have done it right.
+        //console.log("test update");
+    }
+
+    function changePokemon() {
+        //newPokemon has a id in it. you can change to dexNum if you want on line 199 (at the time of me writing this note)
+        //I dont know the API request to change the pokemon in list.
+        //This functtion needs to refresh page on exec, to grab new mon from api.
+        //console.log("test change");
     }
 
     function doSomething() {
@@ -81,6 +247,8 @@ export function TeamBuilder() {
     function dontSomething() {
 
     }
+
+    
 
     useEffect(() => {
         async function GetAnalysis() {
@@ -97,11 +265,11 @@ export function TeamBuilder() {
         let analysisText = "";
         if(analysis !== "" && runAnalysis !==0) {
             analysisText = analysis.map((a) => (
-                <>
+                <div className="analyis">
                     <p>{a.ok}</p>
                     <p>{a.warning}</p>
                     <p>{a.error}</p>
-                </>
+                </div>
             ));
         }
             
@@ -110,17 +278,10 @@ export function TeamBuilder() {
         );
     }
 
-    //This is what will call whatever we bring up to pick the next pokemon. How exactly thats done will have to wait until i know what that will look like.
-    //Should be changed to a react button
-    function AddPokemon() {
-        if(pokemonSelected.length < 6)
-        return (
-            <Button variant="outline-primary" className="team_builder_add_pokemon" onClick={() => dontSomething()}>Add Pokemon</Button>
-        );
-    }
+    
     function AnalyzeButton() {
         return (
-            <Button variant="outline-primary" className="team_builder_add_pokemon" onClick={() => doSomething()}>Analyze</Button>
+            <Button variant="outline-secondary" className="team_builder_add_pokemon analyis" onClick={() => doSomething()}>Analyze</Button>
         );
     }
 
@@ -128,11 +289,12 @@ export function TeamBuilder() {
         <div className="team_builder_main">
             <div className="team_lineup">
                 <TeamDisplay />
-                {AddPokemon()}
             </div>
             <div className='team_analysis'>
+                
                 <AnalyzeButton />
                 <Analysis />
+                <PokeCard />
             </div>
         </div>
     );
